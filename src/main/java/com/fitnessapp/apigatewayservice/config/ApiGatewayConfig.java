e292f11784b3f36501811a9979886fbe20aa5686
Rokszin Roland <rokszinroland@gmail.com>
2026-09-19 14:48:49 +0200

    api-gateway-service bootstrapping

---
+++ src/main/java/com/fitnessapp/apigatewayservice/config/ApiGatewayConfig.java
@@ -0,0 +1,20 @@
+package com.fitnessapp.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApiGatewayConfig {
+
+    @Bean
+    public RouteLocator routes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route("user-service", route -> route
+                        .path("/api/v1/users/**")
+                        .filters(filter -> filter.stripPrefix(2))
+                        .uri("lb://user-service"))
+                .build();
+    }
+}
